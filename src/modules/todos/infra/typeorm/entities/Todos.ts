f977a27cbbdec5bb6152aab89f2075b651e9923e
Rodrigo Demetrio Palma <rodrigo.demetrio.palma@gmail.com>
2020-12-14 22:01:09 -0300

    criacao da entidade Todos

---
+++ src/modules/todos/infra/typeorm/entities/Todos.ts
@@ -0,0 +1,32 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+
+@Entity('todos')
+class Todos {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  title: string;
+
+  @Column()
+  body: string;
+
+  @Column('timestamp with time zone')
+  date: Date;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+
+export default Todos;
